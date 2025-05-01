1a12f2dbba1843c55579e451e5ac08518cfc4a52
Aldy0226 <putra.jaya.ft24@mail.umy.ac.id>
2025-05-01 18:55:04 +0700

    Membuat inputData pada modifire private

--- TugasPemdas.cpp
+++ TugasPemdas.cpp
@@ -7,6 +7,19 @@ class Segitiga
     int alas;
     int tinggi;
 
+    void inputData()
+    {
+        cout << "Masukkan Alas Segitiga \t= ";
+        cin >> alas;
+        cout << "Masukkan Tinggi Segitiga \t= ";
+        cin >> tinggi;
+    }
+    private :
+        float luas(int a, int t) 
+        {
+            return 0.5 * a * t;
+        }
+
     
     
 };
